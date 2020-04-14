04807755d79323d6a3f7847d7ccd7b08cef57ffc
sssungeun <sssungeun@naver.com>
2020-04-14 16:22:41 +0900

    학원커밋3-3

--- a002_layout/src/main/java/com/lec/a002_layout/MainActivity.java
+++ a002_layout/src/main/java/com/lec/a002_layout/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear3);
+        setContentView(R.layout.constraint1);
 
         int ss = 100;
         String tt = "하하호호";
