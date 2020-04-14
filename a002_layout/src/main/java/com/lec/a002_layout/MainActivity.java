1595eaabfc6b45056303bca33d8b1f6ab79e66fb
sssungeun <sssungeun@naver.com>
2020-04-14 10:21:29 +0900

    initial try

---
+++ a002_layout/src/main/java/com/lec/a002_layout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lec.a002_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear3);
+
+
+    }
+
+}
