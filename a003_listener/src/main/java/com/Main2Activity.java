e6d8cee69227dc9b60ca90c5b326ac793ad1ebcc
sssungeun <sssungeun@naver.com>
2020-04-14 17:40:28 +0900

    과제하자

---
+++ a003_listener/src/main/java/com/Main2Activity.java
@@ -0,0 +1,16 @@
+package com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lec.android.a003_listener.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
